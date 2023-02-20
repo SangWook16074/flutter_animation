674ca5b3c0b7926223ed4f3caa960e15eb702f59
HanSangWook <qazws78941@gmail.com>
2023-02-20 20:54:18 +0900

    기본 실습 구성

---
+++ lib/src/ui/app.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class DraggableCard extends StatefulWidget {
+  final Widget child;
+  const DraggableCard({required this.child});
+
+  @override
+  _DraggableCardState createState() => _DraggableCardState();
+}
+
+class _DraggableCardState extends State<DraggableCard> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      child: Card(
+        child: widget.child,
+      ),
+    );
+  }
+}
